a0df8dd6051633efb7d3cd67fccfb7337b719964
niklasanesen <anesen.niklas@gmail.com>
2026-09-21 18:26:34 +0200

    oauth client

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,63 @@
 use axum::{Router, routing::get};
+use oauth2::{AuthUrl, ClientId, ClientSecret, EndpointNotSet, EndpointSet, RedirectUrl, TokenUrl};
+
+type BasicClient = oauth2::basic::BasicClient<
+    EndpointSet,
+    EndpointNotSet,
+    EndpointNotSet,
+    EndpointNotSet,
+    EndpointSet,
+>;
+
+#[derive(Clone)]
+struct Ctx {
+    google: BasicClient,
+    github: BasicClient,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    dotenvy::dotenv().ok();
+
+    let api_url = std::env::var("API_URL").unwrap_or_else(|_| "http://127.0.0.1:8080".to_owned());
+
+    let google = oauth_client(
+        std::env::var("GOOGLE_CLIENT_ID")?,
+        std::env::var("GOOGLE_CLIENT_SECRET")?,
+        "https://accounts.google.com/o/oauth2/v2/auth".to_owned(),
+        "https://oauth2.googleapis.com/token".to_owned(),
+        format!("{api_url}/auth/google/callback"),
+    )?;
+
+    let github = oauth_client(
+        std::env::var("GITHUB_CLIENT_ID")?,
+        std::env::var("GITHUB_CLIENT_SECRET")?,
+        "https://github.com/login/oauth/authorize".to_owned(),
+        "https://github.com/login/oauth/access_token".to_owned(),
+        format!("{api_url}/auth/github/callback"),
+    )?;
+
+    let ctx = Ctx { google, github };
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .with_state(ctx);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await?;
     println!("listening to http://{}", listener.local_addr()?);
     Ok(axum::serve(listener, app).await?)
+}
+
+fn oauth_client(
+    client_id: String,
+    client_secret: String,
+    auth_url: String,
+    token_url: String,
+    redirect_url: String,
+) -> anyhow::Result<BasicClient> {
+    Ok(oauth2::basic::BasicClient::new(ClientId::new(client_id))
+        .set_client_secret(ClientSecret::new(client_secret))
+        .set_auth_uri(AuthUrl::new(auth_url)?)
+        .set_token_uri(TokenUrl::new(token_url)?)
+        .set_redirect_uri(RedirectUrl::new(redirect_url)?))
 }
